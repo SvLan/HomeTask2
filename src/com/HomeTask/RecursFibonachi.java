e814951c61bd8ef1c8953231f10a3546625772a4
SvLan <sharrott@mail.ru>
2016-07-09 10:57:49 +0300

    2recurs

--- src/com/HomeTask/RecursFibonachi.java
+++ src/com/HomeTask/RecursFibonachi.java
@@ -4,4 +4,22 @@ package com.HomeTask;
  * Created by Администратор on 30.06.2016.
  */
 public class RecursFibonachi {
+    static int counter = 0;
+    static int fib(int n) {
+        if (n == 4) {
+            counter++;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fib(n - 2) + fib(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int fibRes = fib(15);
+
+        System.out.println("15-ый элемент Фибоначчи = " + fibRes);
+        System.out.println("Для вычисления 15-го, четвертый нужно посчитать  " + counter + " раз");
+    }
 }
+
